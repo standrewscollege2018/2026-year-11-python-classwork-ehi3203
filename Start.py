662c0f308423a3eb36500f27d79d81cbef88bef2
ehi3203 <ehi3203@stacmail.net>
2026-02-06 12:08:07 +1300

    print

---
+++ Start.py
@@ -0,0 +1,6 @@
+''' This program demonstrates print(),data types, variables, inputs and f-strings '''
+
+# print() is a function that outputs whatever is inside the brackets
+# numbers ca be included directly 
+
+print(123)
